585a781b32b7cf39bdc7518811cb82f558a90390
kankkuna <karina842@hotmail.com>
2019-04-28 22:59:16 -0500

    se modifican proyectos en version mobile y desktop

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.parallax').parallax();
 });
 
-$(document).ready(function(){
-    $('.tooltipped').tooltip({
-      delay: 50});
-      $('.carousel.carousel-slider').carousel({
-        fullWidth: true,
-        indicators: true,
-      });
-    });
+$(document).ready(function () {
+  $('.tooltipped').tooltip({
+    delay: 50
+  });
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+  });
+});
+
+$(document).ready(function () {
+  $('.modal').modal();
+});
